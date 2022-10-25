0bfd6c6e9bd2ca5fe72605796c14531a16064e23
carlos <si>
2022-10-25 12:43:08 -0500

    se envia la primera version

---
+++ Models/usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace proyecto24BM.Models
+{
+    public class usuario
+    {
+        [key]
+        public int PkUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        
+        [ForeignKey("Roles")]
+        public int Fkrol { get; set; }
+        public roles Roles { get; set; }
+    }
+}
